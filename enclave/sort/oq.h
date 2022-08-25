b2318a5b38e2579f04d493f6b9d21167ea1fa811
Bingnan Chen <bingnan.chen@connect.ust.hk>
2022-08-25 02:46:53 +0000

    Fix oq.h

--- enclave/sort/oq.h
+++ enclave/sort/oq.h
@@ -11,6 +11,7 @@ int SampleLoose(int inStructureId, int samplesId);
 int quantileCalT(int *samples, int start, int end, int p, int *trustedM1);
 int quantileCalL(int sampleId, int start, int end, int p, int *trustedM1);
 std::pair<int, int> MultiLevelPartitionT(int inStructureId, int *samples, int sampleSize, int p, int outStructureId1);
+std::pair<int, int> MultiLevelPartitionL(int inStructureId, int sampleId, int sampleSize, int p, int outStructureId1);
 int ObliviousTightSort(int inStructureId, int inSize, int sampleId, int outStructureId1, int outStructureId2);
 std::pair<int, int> ObliviousLooseSort(int inStructureId, int inSize, int sampleId, int outStructureId1, int outStructureId2);
 
